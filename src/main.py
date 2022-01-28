762464a285d02dd3cd3582300a2a4f959f12e9d4
lukemoore495 <lukemoore495@gmail.com>
2022-01-28 10:52:44 -0500

    Add mqtt subscriber/publisher functionality

---
+++ src/main.py
@@ -0,0 +1,12 @@
+﻿import os
+from mqtt import *
+
+def on_message(client, userdata, msg):
+    print(f"Received '{msg.payload.decode()}' from '{msg.topic}' topic")
+
+subscriber = subscribe(on_message, Topic.ALERT_ON)
+
+publisher = getClient()
+publish(publisher, "", Topic.ALERT_ON)
+publish(publisher, "test message", Topic.ALERT_ON)
+publish(publisher, "an alert happened", Topic.ALERT_ON)
