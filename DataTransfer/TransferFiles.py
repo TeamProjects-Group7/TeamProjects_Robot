c87ce44675d61c49354266aeac160c571f2c6f7d
A-Cain <cain.80@wright.edu>
2022-02-14 16:18:31 -0500

    Added environment variable for credentials in yml file

--- DataTransfer/TransferFiles.py
+++ DataTransfer/TransferFiles.py
@@ -1,11 +1,18 @@
 #python 3.6
 from github import Github
-token = "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"
-git_user = Github(token)
+DATA_ACCESS = ""
+
+def uploadFile(filename):
+    folder_prefix = "Data/"
+    filePath = folder_prefix + filename
+    with open(filePath, 'r') as file:
+        content = file.read()
+    repo.create_file(file, "creating test file", "", branch="main")
+
+
+git_user = Github(DATA_ACCESS)
 repo = git_user.get_user().get_repo("TeamProjectsIIData")
-folder_prefix = "Data/"
-test_file = folder_prefix + "TestData.txt"
-contents = repo.get_contents("")
-with open('Data/TestData.txt', 'r') as file:
-    content = file.read()
-repo.create_file(test_file, "creating test file", "test", branch="main")
+uploadFile("TestData.txt")
+
+
+
