f91786285196022e22c9027c52041d2e82df0287
A-Cain <78111116+A-Cain@users.noreply.github.com>
2022-02-15 07:20:59 -0800

    Connect  robot to remote repot demonstrate ability to upload data (#30)
    * Demonstrating Hello World
    
    * Added script that uploads a test file to the repository for the data
    
    * Added environment variable for credentials in yml file
    
    * added workflow to do variable substitution with secrets (hopefully)
    
    * removed newline at the end of the helloworld
    
    * Fixed both ymls and fixed error in TransferFiles.py
    
    * Fixed yml file path to file
    
    * using json file to store variables

--- DataTransfer/TransferFiles.py
+++ DataTransfer/TransferFiles.py
@@ -1,5 +1,8 @@
 #python 3.6
 from github import Github
+import json
+f = open("Variables.json")
+variables = json.load(f)
 
 def uploadFile(filename):
     folder_prefix = "Data/"
@@ -8,10 +11,9 @@ def uploadFile(filename):
         content = file.read()
     repo.create_file(file, "creating test file", "", branch="main")
 
-
-git_user = Github(DATA_ACCESS)
+git_user = Github(variables["DATA_ACCESS"])
 repo = git_user.get_user().get_repo("TeamProjectsIIData")
 uploadFile("TestData.txt")
-
+f.close()
 
 
