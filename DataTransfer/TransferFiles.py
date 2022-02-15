d4aad751ee3222557221b37f1e2039a85a44579f
A-Cain <cain.80@wright.edu>
2022-02-15 10:16:24 -0500

    using json file to store variables

--- DataTransfer/TransferFiles.py
+++ DataTransfer/TransferFiles.py
@@ -1,6 +1,9 @@
 #python 3.6
 from github import Github
-DATA_ACCESS = ""
+import json
+f = open("Variables.json")
+variables = json.load(f)
+
 def uploadFile(filename):
     folder_prefix = "Data/"
     filePath = folder_prefix + filename
@@ -8,10 +11,9 @@ def uploadFile(filename):
         content = file.read()
     repo.create_file(file, "creating test file", "", branch="main")
 
-
-git_user = Github(DATA_ACCESS)
+git_user = Github(variables["DATA_ACCESS"])
 repo = git_user.get_user().get_repo("TeamProjectsIIData")
 uploadFile("TestData.txt")
-
+f.close()
 
 
