b919528c7ff44c8bf1e42684f83047c840ca1ea6
lukemoore495 <lukemoore495@gmail.com>
2022-02-22 00:06:25 -0500

    Add a opencv motion detector for a static camera

--- src/main.py
+++ src/main.py
@@ -1,12 +1,13 @@
 ﻿import os
 from mqtt import *
+from static_detect import *
+import time
 
-def on_message(client, userdata, msg):
-    print(f"Received '{msg.payload.decode()}' from '{msg.topic}' topic")
+detector = MotionDetector(2.5, 32)
+time.sleep(5)
+detector.scan(25)
+time.sleep(10)
+print("can do some stuff in between scans")
+detector.scan(25)
 
-subscriber = subscribe(on_message, Topic.ALERT_ON)
 
-publisher = getClient()
-publish(publisher, "", Topic.ALERT_ON)
-publish(publisher, "test message", Topic.ALERT_ON)
-publish(publisher, "an alert happened", Topic.ALERT_ON)
