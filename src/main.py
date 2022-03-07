bc8089ae4805307472ee026d464d95562dceb5de
lukemoore495 <lukemoore495@gmail.com>
2022-03-06 23:03:46 -0500

    Added an idle state for the raspberry pi

--- src/main.py
+++ src/main.py
@@ -1,13 +1,36 @@
-﻿import os
+﻿from paho.mqtt.client import MQTTMessage
 from mqtt import *
-from static_detect import *
+from MotionDetector import *
 import time
 
-detector = MotionDetector(2.5, 32)
-time.sleep(5)
-detector.scan(25)
-time.sleep(10)
-print("can do some stuff in between scans")
-detector.scan(25)
+idle = True
+def set_idle(client, userdata, msg: MQTTMessage):
+    global idle
+    status = msg.payload.decode().lower()
+    if(status != "false" and status != "true"):
+        return;
+
+    if status == "false":
+        idle = False
+    else:
+        idle = True
+    print(f"Set idle state to {idle}\n")
+
+def main():
+    global idle
+
+    #rather than looping here we will probably check
+    #"idle" at various points in the application and
+    #exit main if its true
+    while idle == False:
+        print("Robot will function while idle is off")
+        time.sleep(5)
 
 
+subscriber = subscribe(set_idle, Topic.ROBOT_IDLE);
+while True:
+    while idle:
+        print("Currently idle. Waiting...")
+        time.sleep(5)
+
+    main()
