824a5eec0b1c93f60da12251b84ac19ceb4b6020
A-Cain <78111116+A-Cain@users.noreply.github.com>
2022-03-08 06:34:39 -0800

    Connect  robot to remote repot demonstrate ability to upload data (#34)
    * Demonstrating Hello World
    
    * Added script that uploads a test file to the repository for the data
    
    * Added environment variable for credentials in yml file
    
    * added workflow to do variable substitution with secrets (hopefully)
    
    * removed newline at the end of the helloworld
    
    * Fixed both ymls and fixed error in TransferFiles.py
    
    * Fixed yml file path to file
    
    * using json file to store variables
    
    * Added ability to commit all files in the data folder to the repo

--- DataTransfer/TransferFiles.py
+++ DataTransfer/TransferFiles.py
@@ -1,19 +1,34 @@
 #python 3.6
 from github import Github
 import json
+import os
 f = open("Variables.json")
 variables = json.load(f)
 
-def uploadFile(filename):
+def uploadFile(filename, repo):
+    repo_files = []
+    repo_content = repo.get_contents("")
+    while repo_content:
+        file_content = repo_content.pop(0)
+        if file_content.type == "dir":
+            repo_content.extend(repo.get_contents(file_content.path))
+        else:
+            repo_files.append(str(file_content).replace('ContentFile(path="','').replace('")',''))
     folder_prefix = "Data/"
     filePath = folder_prefix + filename
     with open(filePath, 'r') as file:
         content = file.read()
-    repo.create_file(file, "creating test file", "", branch="main")
+    if filePath in repo_files:
+        repo_contents = repo.get_contents(filePath)
+        repo.update_file(repo_contents.path, "committing files", content, repo_contents.sha, branch="main")
+    else:
+        repo.create_file(filePath, "comitting files", content, branch="main")
 
-git_user = Github(variables["DATA_ACCESS"])
+git_user = Github("Ask Asher for the access code")
 repo = git_user.get_user().get_repo("TeamProjectsIIData")
-uploadFile("TestData.txt")
+local_files = os.listdir("Data/")
+for filename in local_files :
+    uploadFile(filename, repo)
 f.close()
 
 
