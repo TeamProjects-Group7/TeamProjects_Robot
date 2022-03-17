e66901d85a07d144e5b4df7f8b7aca1b76038659
A-Cain <cain.80@wright.edu>
2022-03-16 22:33:24 -0400

    Re-made the Camera.py to include code to be used as a thread. Assumption being that the main thread that keeps track of whether there is an alert will utilize this code in a thread May require changes as I'm not the most experienced with python threads

--- Routines/Camera.py
+++ Routines/Camera.py
@@ -1,15 +1,24 @@
 import picamera
 import time
 import os
-
-def Record():
-    camera = picamera.PiCamera()
-    #camera is upside down by default apparently
-    camera.vflip = True
-    data_files = 0
-    for files in os.walk("Data"):
-        data_files += 1
-    footage_name = "Data/Footage" + (data_files+1)
-    camera.start_recording(footage_name)
-    time.sleep(300)
-    camera.stop_recording()
+import threading
+data_files = 0
+camera = picamera.PiCamera()
+alert = 0
+lock = threading.Condition()
+class Camera(threading.Thread()):
+    #inittializing thread
+    def __init__(self, thread_name, thread_ID):
+        threading.Thread.__init__(self)
+        camera.vflip = True
+        data_files = 0
+        for files in os.walk("Data"):
+            data_files += 1
+    #run thread and record
+    def run(self):
+        global alert
+        footage_name = "Data/Footage" + (data_files+1)
+        camera.start_recording(footage_name)
+        while (alert):
+            time.sleep(5)
+        camera.stop_recording()
