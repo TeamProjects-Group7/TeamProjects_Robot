b42399552e53f5c84408e3b697897530e3516e0c
lukemoore495 <lukemoore495@gmail.com>
2022-04-12 09:57:55 -0400

    Added main loop

--- src/main.py
+++ src/main.py
@@ -2,8 +2,15 @@
 from mqtt import *
 from MotionDetector import *
 import time
+from Camera import *
+from Microphone import *
+from DataTransfer.TransferFiles import *
 
 idle = True
+alerted = False
+camera = Camera()
+mic = Microphone()
+
 def set_idle(client, userdata, msg: MQTTMessage):
     global idle
     status = msg.payload.decode().lower()
@@ -16,21 +23,50 @@ def set_idle(client, userdata, msg: MQTTMessage):
         idle = True
     print(f"Set idle state to {idle}\n")
 
+def on_alert_off(client, userdata, msg:MQTTMessage):
+    camera.stop()
+    mic.stop_recording()
+    #upload all files to github
+    alerted = False
+
+def raise_alert(publisher):
+    alerted = True
+    publish(publisher, "", Topic.ALERT_ON)
+    camera.start()
+    mic.start_recording()
+    count = 0
+    while(alerted & count <= 100):
+        time.sleep(5)
+        count = count + 1
+    if(count >= 100):
+        camera.stop()
+        mic.stop_recording()
+        #upload all files to github
+        alerted = False
+
+
 def main():
     global idle
-
+    global alerted
     #rather than looping here we will probably check
     #"idle" at various points in the application and
     #exit main if its true
     while idle == False:
         print("Robot will function while idle is off")
-        time.sleep(5)
+        if (mic.idleListen() > 20):
+            raise_alert(publisher)
 
 
 subscriber = subscribe(set_idle, Topic.ROBOT_IDLE);
+publisher = getClient()
+
 while True:
     while idle:
         print("Currently idle. Waiting...")
         time.sleep(5)
+       
+            
+            
+
 
     main()
